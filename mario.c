7183a4bdd0d721aa173fb36491e31203c99c55da
JorgeVenegas <JorgeVenegas@users.noreply.github.com>
2020-08-22 00:37:23 +0000

    automated commit by check50 [check50=True]

---
+++ mario.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) 
+{
+    int height = get_int("Height: \n"); 
+    for (int i = 1; i <= height; i++)
+    {
+        for(int j = 0; j < (height - i); j++) 
+        {
+        printf(" ");    
+        }
+        for(int k = 0; k < i; k++) 
+        {
+        printf("#");    
+        }
+        printf("  ");    
+        for(int k = 0; k < i; k++) 
+        {
+        printf("#");    
+        }
+        printf("\n");    
+    }
+}
