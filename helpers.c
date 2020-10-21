23ba4bdcaf86335c2e2bcf2d16387615621f6ee9
JorgeVenegas <JorgeVenegas@users.noreply.github.com>
2020-10-21 05:56:23 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -1,10 +1,11 @@
 #include "helpers.h"
 #include <math.h>
+#include <stdio.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    float avg;
+    float avg = 0;
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
