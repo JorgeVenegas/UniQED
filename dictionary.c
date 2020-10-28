fbe8f3d805629c44b3a6b88548776cb0762c281c
JorgeVenegas <JorgeVenegas@users.noreply.github.com>
2020-10-28 18:54:17 +0000

    automated commit by submit50

--- dictionary.c
+++ dictionary.c
@@ -18,7 +18,7 @@ typedef struct node
 node;
 
 // Number of buckets in hash table
-const unsigned int N = 10000;
+const unsigned int N = 1000;
 
 // Number of words in dictionary.
 int words = 0;
@@ -45,10 +45,10 @@ bool check(const char *word)
 }
 
 // Hashes word to a number
-unsigned int hash(const char *word) 
+unsigned int hash(const char *word)
 {
     long sum = 0, mul = 1;
-    for (int i = 0; i < strlen(word); i++) 
+    for (int i = 0; i < strlen(word); i++)
     {
         mul = (i % 4 == 0) ? 1 : mul * 256;
         sum += tolower(word[i]) * mul;
