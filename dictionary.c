a08b82ea95a428800d1269c6f6996703adc44d55
JorgeVenegas <JorgeVenegas@users.noreply.github.com>
2020-10-28 18:06:29 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -46,12 +46,14 @@ bool check(const char *word)
 
 // Hashes word to a number
 unsigned int hash(const char *word) {
-  long sum = 0, mul = 1;
-  for (int i = 0; i < strlen(word); i++) {
-    mul = (i % 4 == 0) ? 1 : mul * 256;
-    sum += word[i] * mul;
-  }
-  return (int)(labs(sum) % N);
+    char *low_word = NULL;
+    strcpy(low_word, word);
+    long sum = 0, mul = 1;
+    for (int i = 0; i < strlen(low_word); i++) {
+        mul = (i % 4 == 0) ? 1 : mul * 256;
+        sum += low_word[i] * mul;
+    }
+    return (int)(labs(sum) % N);
 }
 
 // Hashes word to a number
