4c582c2fadf6082d347ee3d9cb4b39ad7ca3809f
JorgeVenegas <JorgeVenegas@users.noreply.github.com>
2020-11-05 03:07:33 +0000

    automated commit by submit50

--- 10.sql
+++ 10.sql
@@ -1,4 +1,4 @@
-SELECT COUNT(name) FROM people WHERE id IN
+SELECT name FROM people WHERE id IN
 (SELECT DISTINCT people.id FROM people
 JOIN directors ON people.id=directors.person_id
 JOIN movies ON movies.id=directors.movie_id
--- 9.sql
+++ 9.sql
@@ -2,4 +2,4 @@ SELECT name FROM people WHERE id IN
 (SELECT DISTINCT people.id FROM people
 JOIN stars ON people.id=stars.person_id
 JOIN movies ON movies.id=stars.movie_id
-WHERE movies.year = 2004 ORDER BY birth);
+WHERE movies.year = 2004) ORDER BY birth;
